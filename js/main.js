de2b74ca4d4dac37ba23af82d0a80466beca2c1f
jiopwls <72438639+jiopwls@users.noreply.github.com>
2020-11-23 17:42:00 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,25 +11,6 @@ $(function () {
       $('.depth2').stop().slideUp(200);
     }
   );
-  var slideIndex = 0;
-  showSlides();
-  
-  function showSlides() {
-      var i;
-      var slides = document.getElementsByClassName("mySlide");
-      var dots = document.getElementsByClassName("dot");
-      for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";  
-      }
-      slideIndex++;
-      if (slideIndex > slides.length) {slideIndex = 1}    
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active2", "");
-      }
-      slides[slideIndex-1].style.display = "block";  
-      dots[slideIndex-1].className += " active2";
-      setTimeout(showSlides, 5000);
-  }
 });
 // 원형 그래프
 $(window).ready(function(){
@@ -61,3 +42,15 @@ function replay(){
  draw(75, '.pie-chart2', '#8b22ff');
  draw(40, '.pie-chart3','#ff0');
 }
+
+$(document).ready(function(){
+  $("#contents1").click(function(){
+      $(".popup").fadeIn()
+  });
+  $("#contents2").click(function(){
+    $(".popup").fadeIn()
+});
+  $(".exit").click(function(){
+      $(".popup").fadeOut();
+  });
+});
