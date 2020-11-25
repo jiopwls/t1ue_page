397d889977ff6ca7074dcbd9c2f8a96a1b20dc62
jiopwls <72438639+jiopwls@users.noreply.github.com>
2020-11-25 15:44:25 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,73 +11,177 @@ $(function () {
       $('.depth2').stop().slideUp(200);
     }
   );
-  var typingBool = false; 
-var typingIdx=0; 
-var liIndex = 0;
-var liLength = $(".typing-txt>ul>li").length;
-
-// 타이핑될 텍스트를 가져온다 
-var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
-//liIndex 인덱스로 구분해 한줄씩 가져옴
-
-typingTxt=typingTxt.split(""); // 한글자씩 잘라 배열로만든다
-
-if(typingBool==false){ // 타이핑이 진행되지 않았다면 
-    typingBool=true; 
-    var tyInt = setInterval(typing,100); // 반복동작 
-} 
-     
-function typing(){ 
-  $(".typing ul li").removeClass("on");
-   $(".typing ul li").eq(liIndex).addClass("on");
-  //현재 타이핑되는 문장에만 커서 애니메이션을 넣어준다.
-  
-  if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
-     $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
-     typingIdx++; 
-   } else{ //한문장이끝나면
-     if(liIndex<liLength-1){
-     //다음문장으로  가기위해 인덱스를 1증가
-       liIndex++; 
-     //다음문장을 타이핑하기위한 셋팅
-        typingIdx=0;
-        typingBool = false; 
-        typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
-       
-     //다음문장 타이핑전 1초 쉰다
-         clearInterval(tyInt);
-          //타이핑종료
-     
-         setTimeout(function(){
-           //1초후에 다시 타이핑 반복 시작
-           tyInt = setInterval(typing,100);
-         },1000);
-        } else if(liIndex==liLength-1){
-          
-         //마지막 문장까지 써지면 반복종료
-           clearInterval(tyInt);
-          
-          //1초후
-          setTimeout(function(){
-            //사용했던 변수 초기화
-            typingBool = false; 
-          liIndex=0;
-          typingIdx=-0;
-            
-            //첫번째 문장으로 셋팅
-          typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
-               //타이핑 결과 모두 지우기
-          $(".typing ul li").html("")
-            
-            //반복시작
-            tyInt = setInterval(typing,100);
-          }, 1000);
-          
-          
-        }
-    } 
-} 
 });
+     
+String.prototype.toKorChars = function() {
+  var cCho = [ 'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' ], 
+  cJung = [ 'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ' ], 
+  cJong = [ '', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ' ], cho, jung, jong; 
+  var str = this, 
+  cnt = str.length, 
+  chars = [], 
+  cCode; 
+  for (var i = 0; i < cnt; i++) { 
+      cCode = str.charCodeAt(i); 
+      if (cCode == 32) { 
+        chars.push(" ");
+        continue;
+      } // 한글이 아닌 경우 
+      if (cCode < 0xAC00 || cCode > 0xD7A3) { 
+          chars.push(str.charAt(i)); continue; 
+          } 
+      cCode = str.charCodeAt(i) - 0xAC00; 
+
+      jong = cCode % 28; 
+      // 종성 
+      jung = ((cCode - jong) / 28 ) % 21 
+
+      // 중성 
+      cho = (((cCode - jong) / 28 ) - jung ) / 21 
+      // 초성 
+
+      //기본 코드 테스트가 ㅌㅔㅅ-ㅌ- 형식으로 저장됨 
+      // chars.push(cCho[cho], cJung[jung]); 
+      // if (cJong[jong] !== '') { 
+      //     chars.push(cJong[jong]); 
+      //     } 
+
+// 이부분을 원하는 방향으로 바꿈.
+      // 테스트라는 문장이 
+      // ㅌ,ㅔ,ㅅ,-,ㅌ,- 형식으로 저장되던 코드를 
+      // ㅌ,테,ㅅ,스,ㅌ,트 형식으로 저장되도록함 (타이핑효과를 위해서)
+      chars.push(cCho[cho]);
+      chars.push(String.fromCharCode( 44032 + (cho * 588) + (jung  * 28)));
+      if (cJong[jong] !== '') { 
+          chars.push(String.fromCharCode( 44032 + (cho * 588) + (jung  * 28) + jong ));
+      }
+     
+  } 
+  
+  return chars; 
+}
+
+
+//타이핑할 문장
+var result1 = "안녕하세요!";
+var result2 = "웹 퍼블리셔라는 꿈을 코딩중인";
+var result3 = "정원진입니다.";
+var typeing1=[],
+    typeing2=[],
+    typeing3=[];
+result1 = result1.split(''); // 한글자씩자름
+result2 = result2.split(''); // 한글자씩자름
+result3 = result3.split(''); // 한글자씩자름
+
+//각글자 초성,중성,종성으로 나눔
+for(var i =0; i<result1.length; i++){
+  typeing1[i]=result1[i].toKorChars();
+}
+for(var i =0; i<result2.length; i++){
+  typeing2[i]=result2[i].toKorChars();
+}
+for(var i =0; i<result3.length; i++){
+  typeing3[i]=result3[i].toKorChars();
+}
+
+//출력할 엘리먼트요소 가져옴 
+var resultDiv1 = document.getElementsByClassName("result1")[0];
+var resultDiv2 = document.getElementsByClassName("result2")[0];
+var resultDiv3 = document.getElementsByClassName("result3")[0];
+
+//
+var text = "";
+var i=0; 
+var j=0; 
+
+//총글자수
+var imax1 = typeing1.length;
+var imax2 = typeing2.length;
+var imax3 = typeing3.length;
+
+//setInterval을 이용해 반복적으로 출력 
+var inter = setInterval(typi,150);
+var inter2;
+var inter3;
+
+
+function typi(){
+  //글자수만큼 반복후 종료
+  $('.result1').addClass('cursor1');
+  if(i<=imax1-1){
+      //각 글자가 초성 중성 종성 순서대로 추가되도록 
+      var jmax1 = typeing1[i].length;
+      $('.result1').innerText = text + typeing1[i][j];
+      j++;
+      if(j==jmax1){
+          text+=  typeing1[i][j-1];//초성중성종성 순서대로 출력된 글자는 저장 ( 다음 글자와 이어붙이기 위해서 )
+          i++;
+          j=0;
+      }
+  } else{
+      clearInterval(inter);
+       text ="";
+      i=0;
+      j=0; 
+ setTimeout(function(){    
+  $('.result1').removeClass("cursor1");
+    setTimeout(function(){             
+      $('.result2').addClass("cursor1");
+       setTimeout(function(){
+          inter2 = setInterval(typi2,150);
+       },400);
+     },300);
+   },400);
+  }
+}
+
+function typi2(){
+  //글자수만큼 반복후 종료 
+
+  if(i<=imax2-1){
+      //각 글자가 초성 중성 종성 순서대로 추가되도록 
+      var jmax2 = typeing2[i].length;
+      $('.result2').innerText = text + typeing2[i][j];
+      j++;
+      if(j==jmax2){
+          text+=  typeing2[i][j-1];//초성중성종성 순서대로 출력된 글자는 저장 ( 다음 글자와 이어붙이기 위해서 )
+          i++;
+          j=0;
+      }
+  } else{
+    clearInterval(inter);
+     text ="";
+    i=0;
+    j=0; 
+setTimeout(function(){    
+  $('.result2').removeClass("cursor1");
+  setTimeout(function(){             
+    $('.result3').addClass("cursor1");
+     setTimeout(function(){
+        inter3 = setInterval(typi3,150);
+     },400);
+   },300);
+ },400);
+}
+}
+
+function typi3(){
+  //글자수만큼 반복후 종료 
+
+  if(i<=imax3-1){
+      //각 글자가 초성 중성 종성 순서대로 추가되도록 
+      var jmax3 = typeing3[i].length;
+      $('.result3').innerText = text + typeing3[i][j];
+      j++;
+      if(j==jmax3){
+          text+=  typeing3[i][j-1];//초성중성종성 순서대로 출력된 글자는 저장 ( 다음 글자와 이어붙이기 위해서 )
+          i++;
+          j=0;
+      }
+  } else{
+      clearInterval(inter);
+  }
+}
 
 
 
@@ -105,14 +209,6 @@ function color1(i, classname,colorname){
        "background":"conic-gradient("+colorname+" 0% "+i+"%, #ffffff "+i+"% 100%)"
   });
 }
-
-
-function replay(){
- draw(80, '.pie-chart1', '#ccc');
- draw(70, '.pie-chart2', '#8b22ff');
- draw(50, '.pie-chart3','#ff0');
-}
-
 $(document).ready(function(){
   $("#contents1").click(function(){
       $(".popup").fadeIn()
